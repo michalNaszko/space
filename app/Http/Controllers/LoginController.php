06294da49a092fe185d182d6275241bf48d3356e
Michal <michal.naszko@gmail.com>
2023-09-13 10:35:29 +0200

    Init

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Client\Factory;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
+
+class LoginController extends Controller
+{
+    /**
+     * Authenticate user.
+     *
+     * @param LoginRequest $request
+     * @return void
+     * @throws ValidationException
+     */
+    private function authenticate(LoginRequest $request) : void
+    {
+        $credentials = $request->getCredentials();
+
+        if(!Auth::validate($credentials)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
+        }
+
+        $user = Auth::getProvider()->retrieveByCredentials($credentials);
+        Auth::login($user);
+    }
+
+    /**
+     * Display login page.
+     *
+     * @return Factory|View
+     */
+    public function show() : Factory|View
+    {
+        return view('signIn');
+    }
+
+    /**
+     * Handle account login request
+     *
+     * @param LoginRequest $request
+     *
+     * @return RedirectResponse|Redirector
+     * @throws ValidationException
+     */
+    public function login(LoginRequest $request): RedirectResponse|Redirector
+    {
+        $this->authenticate($request);
+        return redirect('/');
+    }
+
+}
---
+++ app/Http/Requests/Register.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class Register extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email:rfc,dns|unique:users,email',
+            // 'username' => 'required|unique:users,username',
+            'password' => 'required|min:8',
+            // 'password_confirmation' => 'required|same:password'
+        ];
+    }
+}
