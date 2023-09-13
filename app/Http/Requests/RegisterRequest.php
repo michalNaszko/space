d68d6618f24512bfb7f9e4c09f1243798d4358dd
Michal <michal.naszko@gmail.com>
2023-09-13 10:45:31 +0200

    Renamed file Register to RegisterRequest

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class Register extends FormRequest
+class RegisterRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
