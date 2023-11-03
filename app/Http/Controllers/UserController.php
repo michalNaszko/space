9b47002d3e97777665de2a7bba7b8c4ed4fc269f
Michal <michal.naszko@gmail.com>
2023-11-03 18:36:40 +0100

    Fetching users from fb working, problem with scrorable multiple requests

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /* request -> { start_idx: x, number: n } */
+    function getUsers(Request $request): string
+    {
+        return User::query()->skip($request->query('start_idx'))
+            ->take($request->query('number'))->get()->toJson();
+    }
+}
